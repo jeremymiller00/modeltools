adc0c37e260e6b7dc121e818125c74185b63cd23
Jeremy <jeremymiller00@gmail.com>
2021-11-11 17:06:40 -0800

    add docstrings; clean functions

--- src/modeltools/trainingtools.py
+++ src/modeltools/trainingtools.py
@@ -16,53 +16,68 @@ from sklearn.metrics import roc_curve, roc_auc_score, precision_recall_curve, av
 class StatsmodelsWrapper(BaseEstimator, RegressorMixin):
     """
     A simple wrapper class to allow Statsmodels linear model to use various functions from sklearn.
-    
     """
 
     def __init__(self, fit_intercept=True, threshold=None, is_logit=False):
+        """
+        Constructor for Statsmodels Wrapper
+
+        @param fit_intercept: To fit the intercept in the linear model or not
+        @type fit_intercept: bool
+        @param threshold: threshold for prediciting the positive class in classification
+        @type threshold: float
+        @param is_logit: True for classification, False for regression
+        @type is_logit: bool
+        """
         self.fit_intercept = fit_intercept
         self.threshold = threshold
         self.is_logit = is_logit
+        self._model = None
+        self._fit_model = None
 
-    def fit(self, X, y, column_names=()):
+    def fit(self, X, y, column_names=[]):
         """
-        Parameters
-        ------------
-        column_names: list
-                It is an optional value, such that this class knows 
-                what is the name of the feature to associate to 
-                each column of X. This is useful if you use the method
-                summary(), so that it can show the feature name for each
-                coefficient
+        Wrapper for the fit function
+        @param X: Model features, aka exogenous variables
+        @type X: array-like
+        @param y: Model labels, aka endogenous variable
+        @type y: array-like
+        @param column_names: option value to associate feature names with columns X for summary
+        @type column_names: list[str]
+        @return: Self
+        @rtype: Self
         """
+
         if self.fit_intercept:
             X = sm.add_constant(X)
 
         # Check that X and y have correct shape
         X, y = check_X_y(X, y)
-        self.X_ = X
-        self.y_ = y
 
         if len(column_names) != 0:
-            cols = column_names.copy()
-            cols = list(cols)
             X = pd.DataFrame(X)
             cols = column_names.copy()
             cols.insert(0, 'intercept')
-            print('X ', X)
             X.columns = cols
 
         if self.is_logit:
-            self.model_ = sm.Logit(y, X)
+            self._model = sm.Logit(y, X)
         else:
-            self.model_ = sm.OLS(y, X)
-        self.results_ = self.model_.fit()
+            self._model = sm.OLS(y, X)
 
-        return self
+        self._fit_model = self._model.fit()
 
     def predict(self, X):
+        """
+        Wrapper for predict function
+        @param X: Features
+        @type X: array-like
+        @return: Model predictions
+        @rtype: array-like
+        """
+
         # Check is fit had been called
-        check_is_fitted(self, 'model_')
+        check_is_fitted(self, '_model')
 
         if self.fit_intercept:
             X = sm.add_constant(X)
@@ -71,28 +86,54 @@ class StatsmodelsWrapper(BaseEstimator, RegressorMixin):
         X = check_array(X)
 
         if self.threshold:
-            return self.results_.predict(X) > self.threshold
+            return self._fit_model.predict(X) > self.threshold
 
-        return self.results_.predict(X)
+        return self._fit_model.predict(X)
 
-    def get_params(self, deep=False):
+    def get_wrapper_params(self):
+        """
+        Return wrapper parameters
+        @return: wrapper parameters
+        @rtype: dict
+        """
         return {
             'fit_intercept': self.fit_intercept,
             'threshold': self.threshold,
             'is_logit': self.is_logit,
         }
 
+    def get_fit_model(self):
+        """
+        Return fit model to remove wrapper class
+        @return: fit model
+        @rtype: Statsmodels fit model
+        """
+        return self._fit_model
+
     def summary(self):
-        print(self.results_.summary())
-
-
-def values_from_dataframe(features: list, label: str, data: pd.DataFrame):
-    X = data[features].values
-    y = data[label].values
-    return X, y
+        """
+        Wrap Statsmodels summary function
+        @return: fit model summary
+        @rtype: str
+        """
+        print(self._fit_model.summary())
 
 
 def loo_cv(X, y, model=LogisticRegression(C=1000000), is_classifier=True):
+    """
+    Convenience wrapper for Leave One Out Classification with Sklearn
+
+    @param X: Features
+    @type X: array-like
+    @param y: Label
+    @type y: array-like
+    @param model: sklearn model class (not yet instantiated)
+    @type model: sklearn model (varies)
+    @param is_classifier: needed for calling correct predicition method
+    @type is_classifier: bool
+    @return: two arrays; true label values, predicted label values
+    @rtype: numpy arrays
+    """
     cv = LeaveOneOut()
     y_true, y_pred = list(), list()
     for train_ix, test_ix in cv.split(X):
@@ -103,11 +144,11 @@ def loo_cv(X, y, model=LogisticRegression(C=1000000), is_classifier=True):
         model.fit(X_train, y_train)
         # evaluate model
         if is_classifier:
-            yhat = model.predict_proba(X_test)
-            y_pred.append(yhat[0][1])
+            y_hat = model.predict_proba(X_test)
+            y_pred.append(y_hat[0][1])
         else:
-            yhat = model.predict(X_test)
-            y_pred.append(yhat)
+            y_hat = model.predict(X_test)
+            y_pred.append(y_hat)
         y_true.append(y_test[0])
 
     y_true = np.array(y_true)
